fb185286fd1cce072ab691d371bf8faf1a07f671
Baracuda1337 <60934310+Baracuda1337@users.noreply.github.com>
2020-06-13 13:54:28 +0300

    Initial commit

---
+++ lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 3680;
+            var hours = n / 3600;
+            var minutes = (n - (hours * 3600)) / 60;
+            var seconds = n % 60;
+
+            Console.WriteLine( "З початку доби пройшло " +
+            $"{hours} годин {minutes} хвилин {seconds} секунд")
+               ;
+        }
+    }
+}   
